d6c0bb48adb438c29087abcf31d6422f013ab478
arun.menon <ntarunmenon@gmail.com>
2016-02-15 20:29:13 +0400

    ProcFile added

--- target/classes/import.sql
+++
@@ -1,3 +0,0 @@
-insert into company (company_id, address, city, country, email, name, phone_number) values (null, 'St:Louis Street,main Streer', 'city', 'India', 'test@test.com', 'Arun Company', '00378645');
-insert into owner (id, name) values (null, 'Arun Menon');
-insert into company_owners (company_id, id) values (1, 1); 
